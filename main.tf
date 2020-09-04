70f40e8d4d62c617e607a12a368a303bd6a16c96
Sukanta Mondal <60913612+mondalsukanta@users.noreply.github.com>
2020-09-03 20:33:00 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_route_table" "prod-route-table" {
 resource "aws_subnet" "subnet-1" {
    vpc_id            = aws_vpc.prod-vpc.id
    cidr_block        = "10.0.1.0/24"
-   availability_zone = "us-east-1a"
+   availability_zone = "us-east-2a"
 
    tags = {
      Name = "prod-subnet"
